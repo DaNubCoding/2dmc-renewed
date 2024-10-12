a037499329a9cd0c734818fb31176af5afb6de88
DaNubCoding <danubcoding@gmail.com>
2024-10-12 09:44:48 -0400

    make Vec unaccessible from constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,16 +1,16 @@
-from src.utils import Vec
+from src.utils import Vec as _Vec
 
 WIDTH, HEIGHT = 1280, 768 # pixels
-SIZE = Vec(WIDTH, HEIGHT) # pixels
+SIZE = _Vec(WIDTH, HEIGHT) # pixels
 
 BLOCK = 64 # pixels
 CHUNK = 8 # blocks
 BLOCKCHUNK = BLOCK * CHUNK # pixels
-BLOCKXY = Vec(BLOCK, BLOCK) # pixels
-CHUNKXY = Vec(CHUNK, CHUNK) # blocks
-BLOCKCHUNKXY = Vec(BLOCKCHUNK, BLOCKCHUNK) # pixels
+BLOCKXY = _Vec(BLOCK, BLOCK) # pixels
+CHUNKXY = _Vec(CHUNK, CHUNK) # blocks
+BLOCKCHUNKXY = _Vec(BLOCKCHUNK, BLOCKCHUNK) # pixels
 REGION = 32 # chunks
-REGIONXY = Vec(REGION, REGION) # chunks
+REGIONXY = _Vec(REGION, REGION) # chunks
 CHUNK_CHECK_MAX_FREQ = 20 # per second
 CHUNK_LOAD_MAX_FREQ = 128 # per second
 AUTOSAVE_INTERVAL = 15 # seconds
