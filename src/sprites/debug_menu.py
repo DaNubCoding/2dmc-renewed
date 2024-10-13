31fe04600657f95c2a8094fddba3969f4e6812b8
DaNubCoding <danubcoding@gmail.com>
2024-10-13 10:23:31 -0400

    merge loaded chunks and pending chunks debug info

--- src/sprites/debug_menu.py
+++ src/sprites/debug_menu.py
@@ -12,7 +12,6 @@ class DebugMenu(Sprite):
         self.data = {
             "FPS": 0,
             "Loaded Chunks": 0,
-            "Pending Chunks": 0,
             "Loaded Regions": 0,
         }
         self.update_time = time.time()
@@ -22,8 +21,7 @@ class DebugMenu(Sprite):
         if time.time() - self.update_time < 0.05: return
         self.update_time = time.time()
         self.data["FPS"] = int(self.game.clock.get_fps())
-        self.data["Loaded Chunks"] = len(self.scene.chunk_manager.loaded_chunks)
-        self.data["Pending Chunks"] = len(self.scene.chunk_manager.pending_positions)
+        self.data["Loaded Chunks"] = f"{len(self.scene.chunk_manager.loaded_chunks)}/{len(self.scene.chunk_manager.loaded_chunks) + len(self.scene.chunk_manager.pending_positions)}"
         self.data["Loaded Regions"] = len(self.scene.chunk_manager.region_data)
 
     def draw(self, screen: pygame.Surface) -> None:
