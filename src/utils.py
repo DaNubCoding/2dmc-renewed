e568be5abb10abc65f5ce928d9af17d2b84de7c3
DaNubCoding <danubcoding@gmail.com>
2024-10-13 20:00:57 -0400

    document all timer classes

--- src/utils.py
+++ src/utils.py
@@ -267,7 +267,13 @@ class Vec(_Vector2, metaclass=_multimeta):
         """Return the hash of the vector."""
         return tuple(self).__hash__()
 
-class Timer:
+class Timer(metaclass=_multimeta):
+    """A timer class that lasts for a certain duration.
+
+    Args:
+        duration: The duration of the timer in seconds.
+    """
+
     def __init__(self, duration: float) -> None:
         self.duration = duration
         self.time = _time()
@@ -275,38 +281,56 @@ class Timer:
 
     @property
     def elapsed(self) -> float:
+        """The time elapsed since the timer started."""
         return _time() - self.time
 
     @property
     def remaining(self) -> float:
+        """The time remaining until the timer is done."""
         return self.duration - self.elapsed
 
     @property
     def progress(self) -> float:
+        """The progress of the timer as a value between 0 and 1."""
         return self.elapsed / self.duration
 
     @property
     def progress_remaining(self) -> float:
+        """The progress remaining until the timer is done as a value between 0
+        and 1.
+        """
         return self.remaining / self.duration
 
     @property
     def done(self) -> bool:
+        """Whether the timer is done."""
         return self.elapsed >= self.duration and not self.paused
 
-    def reset(self, duration: float = None) -> None:
-        if duration is not None:
-            self.duration = duration
+    def reset(self) -> None:
+        """Reset the timer to the original duration."""
+        self.time = _time()
+
+    def reset(self, duration: float) -> None:
+        """Reset the timer to a new duration.
+
+        Args:
+            duration: The new duration of the timer in seconds.
+        """
+        self.duration = duration
         self.time = _time()
 
     def pause(self) -> None:
+        """Pause the timer."""
         self.duration -= self.elapsed
         self.paused = True
 
     def resume(self) -> None:
+        """Resume the timer."""
         self.time = _time()
         self.paused = False
 
     def toggle(self) -> None:
+        """Toggle the timer between paused and resumed."""
         if self.paused:
             self.resume()
         else:
@@ -319,9 +343,19 @@ class Timer:
         return f"Timer({self.duration})"
 
     def __bool__(self) -> bool:
+        """Whether the timer is running."""
         return not self.done
 
-class LoopTimer(Timer):
+class LoopTimer(Timer, metaclass=_multimeta):
+    """A timer class that lasts for a certain duration and loops a certain
+    number of times.
+
+    Args:
+        duration: The duration of the timer in seconds.
+        max_loops: The maximum number of loops the timer can do. If this is -1,
+            the timer will loop infinitely.
+    """
+
     def __init__(self, duration: float, max_loops: int = -1) -> None:
         super().__init__(duration)
         self.max_loops = max_loops
@@ -329,6 +363,9 @@ class LoopTimer(Timer):
 
     @property
     def done(self) -> bool:
+        """Whether the timer is done. If the timer is done, it will be reset if
+        it hasn't reached the max number of loops yet.
+        """
         if self.paused: return False
         if not super().done: return False
 
@@ -342,7 +379,20 @@ class LoopTimer(Timer):
         # frame if it hasn't reached the max number of loops yet.
         return True
 
-    def reset(self, duration: float = None) -> None:
+    def reset(self) -> None:
+        """Reset the timer to the original duration and reset the number of
+        loops.
+        """
+        super().reset()
+        self.loops = 0
+
+    def reset(self, duration: float) -> None:
+        """Reset the timer to the original duration and reset the number of
+        loops.
+
+        Args:
+            duration: The new duration of the timer in seconds.
+        """
         super().reset(duration)
         self.loops = 0
 
@@ -353,12 +403,35 @@ class LoopTimer(Timer):
         return f"LoopTimer({self.duration}, {self.max_loops})"
 
 class PreciseLoopTimer(LoopTimer):
+    """A more precise version of the loop timer that accounts for the time
+    elapsed between frames, and corrects the timer to start from the correct
+    time in the next frame. In the case of timer durations shorter than the
+    frame time, this timer will loop the correct number of times, while the
+    regular loop timer will not.
+
+    The number of loops that have elapsed since the last frame is stored in the
+    `subframe_loops` attribute, which may be accessed after the timer is done
+    to get the number of loops that have elapsed since the last frame. This
+    attribute may be used to perform the action multiple times in case the
+    timer finished more than once in a single frame.
+
+    Args:
+        duration: The duration of the timer in seconds.
+        max_loops: The maximum number of loops the timer can do. If this is -1,
+            the timer will loop infinitely.
+    """
+
     def __init__(self, duration: float, max_loops: int = -1) -> None:
         super().__init__(duration, max_loops)
         self.subframe_loops = 0
 
     @property
     def done(self) -> bool:
+        """Whether the timer is done. If the timer is done, it will be reset if
+        it hasn't reached the max number of loops yet. The number of loops that
+        have elapsed since the last frame is stored in the `subframe_loops`
+        attribute.
+        """
         if self.paused: return False
 
         # The number of loops that have elapsed since the last frame.
