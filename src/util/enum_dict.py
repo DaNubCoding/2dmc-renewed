dcb14a9fb5b669f5532f0fd5b5d088ab5a7d3d25
DaNubCoding <danubcoding@gmail.com>
2024-10-19 23:45:39 -0400

    rename IntEnumDict.enum to _enum_type signifying private

--- src/util/enum_dict.py
+++ src/util/enum_dict.py
@@ -19,26 +19,26 @@ class IntEnumDict(Generic[KT, VT], dict[KT, VT], metaclass=multimeta):
 
     def __init__(self, source: dict[KT, VT]) -> None:
         super().__init__(source)
-        self.enum = type(next(iter(source)))
+        self._enum_type = type(next(iter(source)))
 
     def __setitem__(self, key: KT, value: VT) -> None:
         super().__setitem__(key, value)
 
     def __setitem__(self, key: str, value: VT) -> None:
         try:
-            key = self.enum[key]
+            key = self._enum_type[key]
         except KeyError:
             raise KeyError(f"Key must be one of [{
-                ', '.join(self.enum.__members__)
+                ', '.join(self._enum_type.__members__)
             }]")
         super().__setitem__(key, value)
 
     def __setitem__(self, key: int, value: VT) -> None:
         try:
-            key = self.enum(key)
+            key = self._enum_type(key)
         except ValueError:
             raise KeyError(f"Key must be one of [{
-                ', '.join(map(str, self.enum))
+                ', '.join(map(str, self._enum_type))
             }]")
         super().__setitem__(key, value)
 
@@ -47,19 +47,19 @@ class IntEnumDict(Generic[KT, VT], dict[KT, VT], metaclass=multimeta):
 
     def __getitem__(self, key: str) -> VT:
         try:
-            key = self.enum[key]
+            key = self._enum_type[key]
         except KeyError:
             raise KeyError(f"Key must be one of [{
-                ', '.join(self.enum.__members__)
+                ', '.join(self._enum_type.__members__)
             }]")
         return super().__getitem__(key)
 
     def __getitem__(self, key: int) -> VT:
         try:
-            key = self.enum(key)
+            key = self._enum_type(key)
         except ValueError:
             raise KeyError(f"Key must be one of [{
-                ', '.join(map(str, self.enum))
+                ', '.join(map(str, self._enum_type))
             }]")
         return super().__getitem__(key)
 
@@ -68,19 +68,19 @@ class IntEnumDict(Generic[KT, VT], dict[KT, VT], metaclass=multimeta):
 
     def __delitem__(self, key: str) -> None:
         try:
-            key = self.enum[key]
+            key = self._enum_type[key]
         except KeyError:
             raise KeyError(f"Key must be one of [{
-                ', '.join(self.enum.__members__)
+                ', '.join(self._enum_type.__members__)
             }]")
         super().__delitem__(key)
 
     def __delitem__(self, key: int) -> None:
         try:
-            key = self.enum(key)
+            key = self._enum_type(key)
         except ValueError:
             raise KeyError(f"Key must be one of [{
-                ', '.join(map(str, self.enum))
+                ', '.join(map(str, self._enum_type))
             }]")
         super().__delitem__(key)
 
@@ -89,14 +89,14 @@ class IntEnumDict(Generic[KT, VT], dict[KT, VT], metaclass=multimeta):
 
     def __contains__(self, key: str) -> bool:
         try:
-            key = self.enum[key]
+            key = self._enum_type[key]
         except KeyError:
             return False
         return super().__contains__(key)
 
     def __contains__(self, key: int) -> bool:
         try:
-            key = self.enum(key)
+            key = self._enum_type(key)
         except ValueError:
             return False
         return super().__contains__(key)
@@ -106,14 +106,14 @@ class IntEnumDict(Generic[KT, VT], dict[KT, VT], metaclass=multimeta):
 
     def get(self, key: str, default: VT = None) -> VT:
         try:
-            key = self.enum[key]
+            key = self._enum_type[key]
         except KeyError:
             return default
         return super().get(key, default)
 
     def get(self, key: int, default: VT = None) -> VT:
         try:
-            key = self.enum(key)
+            key = self._enum_type(key)
         except ValueError:
             return default
         return super().get(key, default)
@@ -123,14 +123,14 @@ class IntEnumDict(Generic[KT, VT], dict[KT, VT], metaclass=multimeta):
 
     def pop(self, key: str, default: VT = None) -> VT:
         try:
-            key = self.enum[key]
+            key = self._enum_type[key]
         except KeyError:
             return default
         return super().pop(key, default)
 
     def pop(self, key: int, default: VT = None) -> VT:
         try:
-            key = self.enum(key)
+            key = self._enum_type(key)
         except ValueError:
             return default
         return super().pop(key, default)
@@ -140,14 +140,14 @@ class IntEnumDict(Generic[KT, VT], dict[KT, VT], metaclass=multimeta):
 
     def setdefault(self, key: str, default: VT = None) -> VT:
         try:
-            key = self.enum[key]
+            key = self._enum_type[key]
         except KeyError:
             return default
         return super().setdefault(key, default)
 
     def setdefault(self, key: int, default: VT = None) -> VT:
         try:
-            key = self.enum(key)
+            key = self._enum_type(key)
         except ValueError:
             return default
         return super().setdefault(key, default)
